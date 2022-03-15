a0ac81fbd7d78961c74130cefc1b28fc42da45b4
john_dev <johnhaimez314@gmail.com>
2022-03-15 17:45:04 +0100

    fixes #7 ; creation dashboard + changement de couleur principal avec les couleurs sur figma

---
+++ lib/pages/private/dashboard.dart
@@ -0,0 +1,35 @@
+import 'package:flutoo/widget_shared/app_bar_flutoo/app_bar_flutoo.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: const AppBarFlutoo(),
+        body: Container(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              tooltip: 'todo',
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              tooltip: 'profil',
+              label: '',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
