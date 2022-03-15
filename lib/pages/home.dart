e797cfb4d83c617dc57de03d56c0dfa3b6081d18
David-Caignaert <davidcaignaert@gmail.com>
2022-03-15 15:44:17 +0100

    feat #10; mise en place des input de connexion

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:woo_widget_connexion/woo_widget_connexion.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,7 +14,25 @@ class _HomeState extends State<Home> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(),
-        body: Container(),
+        body: Center(
+          
+          child: SizedBox(
+            width: 400,
+            child: Column(
+              children: [
+                InputConnexion(
+                  emaillabelText: "Email",
+                  pwslabelText: "Mot de passe",
+                  emailmargin: const EdgeInsets.all(20),
+                  pwsmargin: const EdgeInsets.all(20),
+                  resultForm: (value) {
+                    print(value);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
