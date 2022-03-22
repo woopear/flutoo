1cd2f8a6aed707efdfb7bccb3bc369bb78779fc1
David-Caignaert <davidcaignaert@gmail.com>
2022-03-22 10:33:58 +0100

    feat #10; renommage de UserModel en UserShema + ajout du constructeur factory et toMap

--- lib/models/user/user_shema.dart
+++ lib/models/user/user_shema.dart
@@ -1,4 +1,4 @@
-class UserModel {
+class UserSchema {
   String? id;
   String? email;
   String? password;
@@ -8,16 +8,41 @@ class UserModel {
   String? pseudo;
   bool? termes;
 
-  UserModel({
+  UserSchema({
     this.id,
-    this.uid,
-    this.email,
-    this.password,
+    required this.uid,
+    required this.email,
+    required this.password,
     this.firstName,
     this.lastName,
     this.pseudo,
-    this.termes,
+    this.termes = false,
   });
 
-  set setUid(value) => uid = value;
+  factory UserSchema.formMap(Map<String, dynamic> data, documentId) {
+    String email = data['email'];
+    String password = data['password'];
+    String uid = data['uid'];
+    String firstName = data['firstName'];
+    String lastName = data['lastName'];
+    String pseudo = data['pseudo'];
+    bool termes = data['termes'];
+
+    return UserSchema(
+      id: documentId,
+      email: email,
+      password: password,
+      uid: uid,
+      firstName: firstName,
+      lastName: lastName,
+      pseudo: pseudo,
+      termes: termes,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'email': email,
+    };
+  }
 }
