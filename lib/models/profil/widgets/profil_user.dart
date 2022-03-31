e7457a788bd9eff0a04efd50b5a41ededec75182
john_dev <johnhaimez314@gmail.com>
2022-03-31 22:05:17 +0200

    creation telechargement image affichage sur mobile et web d'une image reste bug sur mobile erreur au telechargement

--- lib/models/profil/widgets/profil_user.dart
+++ lib/models/profil/widgets/profil_user.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutoo/config/routes/routes.dart';
 import 'package:flutoo/models/auth/auth_state.dart';
 import 'package:flutoo/models/profil/widgets/profil_user_card/profil_user_card.dart';
 import 'package:flutoo/models/profil/widgets/profil_user_card/profil_user_update/profil_user_update.dart';
+import 'package:flutoo/models/upload/upload.dart';
+import 'package:flutoo/models/user/user_shema.dart';
 import 'package:flutoo/models/user/user_state.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,6 +21,8 @@ class ProfilUser extends ConsumerStatefulWidget {
 class _ProfilUserState extends ConsumerState<ProfilUser> {
   bool seeUpdate = false;
   bool seeUpdateAvatar = false;
+  dynamic file;
+  String? extension;
 
   /// affiche/cache volet update user
   void openCloseUpdated() {
@@ -38,10 +45,63 @@ class _ProfilUserState extends ConsumerState<ProfilUser> {
     Navigator.pushNamed(context, Routes().home);
   }
 
+  /// selection du fichier à télécharger
+  Future<void> selectAvatar() async {
+    /// si web sinon le reste
+    if (kIsWeb) {
+      /// creation conteneur pour fichier
+      final picker = await FilePicker.platform.pickFiles();
+      if (picker == null) return;
+
+      /// on recupere le path
+      setState(() {
+        file = picker.files.first.bytes;
+        extension = picker.files.first.extension;
+      });
+    } else {
+      /// creation conteneur pour fichier
+      final picker = await FilePicker.platform.pickFiles(allowMultiple: false);
+      if (picker == null) return;
+
+      /// on recupere le fichier
+      setState(() {
+        file = File(picker.files.single.path!);
+      });
+    }
+  }
+
+  /// telecharge l'image dans firestorage
+  Future<void> uploadAvatar(UserSchema user) async {
+    String? url;
+
+    /// upload + recupere url
+    if (file != null) {
+      /// upload + recuperation de l'url
+      url = await ref
+          .watch(uploadFileChange)
+          .uploadAvatar(file, user.uid!, extension!);
+
+      /// on affecte l'url à avatar du user
+      user.avatar = url;
+
+      /// modifier user pour ajouter url à avatar
+      await ref.watch(userState).updateUser(user.id!, user);
+
+      /// reset et ferme le volet update image
+      file = null;
+      openCloseUpdatedAvatar();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     /// on recupere le user connecté
     final user = ref.watch(userCurrent);
+    final avatar = user!.avatar;
+
+    /// message de selection de fichier
+    final fileName =
+        file != null ? '1 fichier prêt à etre télécharger' : 'Aucun fichier';
 
     return Center(
       child: Column(
@@ -55,42 +115,101 @@ class _ProfilUserState extends ConsumerState<ProfilUser> {
             ),
           ),
 
-          /// image / avatar
+          /// cadre image user
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Icon(Icons.person, size: 100),
-              seeUpdateAvatar
-                  ? Container(
-                      child: Align(
-                        child: IconButton(
-                          iconSize: 15,
-                          alignment: const Alignment(0, -4),
-                          onPressed: () {
-                            openCloseUpdatedAvatar();
-                          },
-                          color: Colors.red,
-                          icon: const Icon(
-                            Icons.close,
+              !seeUpdateAvatar
+
+                  /// image
+                  ? Align(
+                      child: Container(
+                      margin: EdgeInsets.zero,
+                      padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(),
+                      child: avatar != "" || avatar != null
+                          ? Image.network(
+                              avatar!.toString(),
+                              width: 80.0,
+                              height: 80.0,
+                            )
+                          : const Icon(Icons.person, size: 100.0),
+                    ))
+
+                  /// btn selected image
+                  : Align(
+                      child: Column(
+                        children: [
+                          /// btn selected image
+                          ElevatedButton(
+                            onPressed: () async {
+                              await selectAvatar();
+                            },
+                            child: const Text(
+                              'Selectionner votre image',
+                              style: TextStyle(fontSize: 14.0),
+                            ),
                           ),
-                        ),
+
+                          /// cadre text info upload / btn telecharger
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 20.0),
+                            child: Column(
+                              children: [
+                                /// text fichier selectionné
+                                Container(
+                                  margin: const EdgeInsets.only(bottom: 20.0),
+                                  child: Text(fileName),
+                                ),
+
+                                /// btn telecharger
+                                ElevatedButton(
+                                  onPressed: file != null
+                                      ? () async {
+                                          await uploadAvatar(user);
+                                        }
+                                      : null,
+                                  child: const Text(
+                                    'Télécharger',
+                                    style: TextStyle(fontSize: 14.0),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  : Container(
-                      child: Align(
-                        child: IconButton(
-                          iconSize: 15,
-                          alignment: const Alignment(0, -4),
-                          onPressed: () {
-                            openCloseUpdatedAvatar();
-                          },
-                          color: Theme.of(context).colorScheme.primary,
-                          icon: const Icon(
-                            Icons.edit,
-                          ),
+                    ),
+
+              /// btn open close update image user
+              Container(
+                margin: seeUpdateAvatar
+                    ? const EdgeInsets.only(left: 20.0)
+                    : EdgeInsets.zero,
+                child: IconButton(
+                  padding: EdgeInsets.zero,
+                  constraints: const BoxConstraints(),
+                  hoverColor: Colors.transparent,
+                  alignment: Alignment.topLeft,
+                  onPressed: () {
+                    openCloseUpdatedAvatar();
+                    setState(() {
+                      file = null;
+                    });
+                  },
+                  color: seeUpdateAvatar
+                      ? Colors.red
+                      : Theme.of(context).colorScheme.primary,
+                  icon: seeUpdateAvatar
+                      ? const Icon(
+                          Icons.close,
+                        )
+                      : const Icon(
+                          Icons.edit,
                         ),
-                      ),
-                    )
+                ),
+              ),
             ],
           ),
 
@@ -154,7 +273,7 @@ class _ProfilUserState extends ConsumerState<ProfilUser> {
                     ? Container(
                         margin: const EdgeInsets.only(bottom: 40, top: 20),
                         child: ElevatedButton(
-                          onPressed: () => {deleteUser(context, user!.id!)},
+                          onPressed: () => {deleteUser(context, user.id!)},
                           child: const Text(
                             'Effacer le compte',
                           ),
--- lib/models/upload/upload.dart
+++ lib/models/upload/upload.dart
@@ -1,21 +1,32 @@
-import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UploadFile extends ChangeNotifier {
   final _firebaseStorage = FirebaseStorage.instance;
 
-  Future<String> upload(File data, String uid) async {
+  Future<String> uploadAvatar(data, String uid, String extension) async {
+    UploadTask? file;
+
     /// on creer la reference de l'image (nom)
     final ref = _firebaseStorage.ref().child('avatars/user-$uid');
-    /// on enregistre sur firebase
-    UploadTask? file = ref.putFile(data);
+    
+    if (kIsWeb) {
+      /// on enregistre sur firebase mode web
+      file = ref.putData(data, SettableMetadata(contentType: 'image/$extension'));
+    } else {
+      /// on enregistre sur firebase mode mobile
+      file = ref.putFile(data);
+    }
 
     /// on recupere le fichier
     final snapshot = await file.whenComplete(() {});
-    /// on recupere l'url pour enregistrer dans le user
-    final url = await snapshot.ref.getDownloadURL();
 
+    /// on recupere l'url et on la retourne pour enregistrer dans le user
+    final url = await snapshot.ref.getDownloadURL();
     return url;
   }
 }
+
+/// state de la class uploadFile
+final uploadFileChange = ChangeNotifierProvider((ref) => UploadFile());
